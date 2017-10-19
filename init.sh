3c3547789ea90c242f124fd7fca357abdc5f1f0b
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2017-10-20 01:32:13 +1100

    Pulling the flayr-mobile-app in the init.sh script

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 GIT_REPOS=(
     "git@github.com:ruchira088/mobile-app-backend.git"
     "git@github.com:ruchira088/airtable-service.git"
+    "git@github.com:ruchira088/flayr-mobile-app.git"
 )
 
 for gitRepo in "${GIT_REPOS[@]}"
